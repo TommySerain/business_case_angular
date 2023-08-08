655d9574fa4f97bb23ff52fbc929b67b82d4a1a6
TommySerain <tommyserain@yahoo.fr>
2023-08-08 11:41:28 +0200

    nav+banniere

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { BanniereComponent } from './banniere/banniere.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    BanniereComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/banniere/banniere.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banniere',
+  templateUrl: './banniere.component.html',
+  styleUrls: ['./banniere.component.css']
+})
+export class BanniereComponent {
+
+}
